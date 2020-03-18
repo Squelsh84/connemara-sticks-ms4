ae55db3b2fbf7015fe2156af14e5c79398e4da9a
squelsh84 <mrmjwalsh@gmail.com>
2020-03-18 12:37:56 +0000

    Updated requirements.txt.

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    length = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
