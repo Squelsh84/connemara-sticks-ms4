94f61c735818442e4e1df3cbbb757b8b7cd54a36
squelsh84 <mrmjwalsh@gmail.com>
2020-03-20 00:19:20 +0000

    added button to add to cart.

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
 ]
