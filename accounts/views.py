9a448f9ecea2974006cc2b947d138b57e2f0d477
squelsh84 <mrmjwalsh@gmail.com>
2020-03-23 23:55:45 +0000

    created a view for signup.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 # Create your views here.
+
+
+def SignUpView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            customer_group = Group.objects.get(name='Customer')
+            customer_group.user_set.add(signup_user)
+            login(request, signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
