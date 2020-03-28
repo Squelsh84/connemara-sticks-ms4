4972eb029c0d8b3cdd1ef10c2d156eab771a3609
squelsh84 <mrmjwalsh@gmail.com>
2020-03-28 21:24:01 +0000

    Moved from home to products the category.

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/', views.productPage, name='product'),
+    path('category/<slug:category_slug>/<slug:product_slug>',
+         views.productPage, name='product_detail'),
+    path('category/<slug:category_slug>',
+         views.allProducts, name='products_by_category'),
     path('products/', views.allProducts, name='products'),
-
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-
+from .models import Category, Product
 # Create your views here.
 
 
-def productPage(request):
-    return render(request, 'product.html')
+def productPage(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(
+            category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
 
 
-def allProducts(request):
-    return render(request, 'products.html')
+def allProducts(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=category_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'products.html', {'category': category_page, 'products': products})
