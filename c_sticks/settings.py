e63d541ef67ca980e7d4e32c12242f0cada610aa
squelsh84 <mrmjwalsh@gmail.com>
2020-03-29 23:42:10 +0100

    added localhost.

--- c_sticks/settings.py
+++ c_sticks/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['connemara-sticks.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST"), '127.0.0.1']
 
 
 # Application definition
