0a86297a5796793eb9f87eb42da895379785363d
squelsh84 <mrmjwalsh@gmail.com>
2020-03-29 21:59:59 +0100

    Created settings.py for aws.

--- c_sticks/settings.py
+++ c_sticks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import dj_database_url
 import os
 
 from os import path
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     'checkout.apps.CheckoutConfig',
     'crispy_forms',
     'django_summernote',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -89,12 +91,9 @@ WSGI_APPLICATION = 'c_sticks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+DATABASES = {'default': dj_database_url.parse(
+    os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
@@ -144,5 +143,21 @@ MEDIA_URL = '/media/'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+STATICFILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
