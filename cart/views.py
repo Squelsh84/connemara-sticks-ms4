177a24a4b39330c6c762575e2341ede6c4f366af
squelsh84 <mrmjwalsh@gmail.com>
2020-03-29 17:25:43 +0100

    Created to create a charge and process it to the stripe account.

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from products.models import Product
 from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
+import stripe
 # Create your views here.
 
 
@@ -46,8 +48,28 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
             counter += cart_item.quantity
     except ObjectDoesNotExist:
         pass
+    stripe.api_key = settings.STRIPE_SECRET
+    stripe_total = int(total * 100)
+    description = 'Connemara Sticks - New Order'
+    data_key = settings.STRIPE_PUBLISHABLE
+    if request.method == 'POST':
+        try:
+            token = request.POST['stripeToken']
+            email = request.POST['stripeEmail']
+            customer = stripe.Customer.create(
+                email=email,
+                source=token
+            )
+            charge = stripe.Charge.create(
+                amount=stripe_total,
+                currency='eur',
+                description=description,
+                customer=customer.id)
 
-    return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter))
+        except stripe.error.CardError as e:
+            return False, e
+
+    return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter,  data_key=data_key, stripe_total=stripe_total, description=description))
 
 
 def cart_remove(request, product_id):
