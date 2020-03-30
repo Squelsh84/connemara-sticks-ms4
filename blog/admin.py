a650e3abc6fd653c7f2a6d992ae6237c027dffb0
squelsh84 <mrmjwalsh@gmail.com>
2020-03-30 17:54:03 +0100

    Added images and changed some styling.

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status', 'created_on')
-    search_fields = ['title', 'content']
+    search_fields = ['title', 'content', 'blog_intro']
     prepopulated_fields = {'slug': ('title',)}
 
     summernote_fields = ('content',)
