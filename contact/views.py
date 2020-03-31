a15521ba8bc99b59cd8490093afd5fc3a32e5cce
squelsh84 <mrmjwalsh@gmail.com>
2020-03-31 01:56:51 +0100

    Fixed errors on messages. added message to base to appear when message is used.

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ContactForm
+from django.core.mail import send_mail
+from django.contrib import messages
 
 
 # Create your views here.
@@ -7,5 +9,23 @@ from .forms import ContactForm
 
 def contact(request):
     form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
 
+            send_mail(
+                'New Contact',
+                'You have a new message.',
+                'mrmjtwalsh@gmail.com',
+                ['mjwalsh@gmail.com', ],
+                fail_silently=False,
+            )
+            messages.success(
+                request,
+                f'Thanks for your message, We will get back soon!'
+            )
+            return redirect('index')
+        else:
+            messages.error(request, f'Sorry something went wrong!')
+    context = {'title': 'Contact', 'form': form}
+    return render(request, 'contact.html', context)
